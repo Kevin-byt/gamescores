d2fee4520fbbc3809dd5a88dc4b56d5d0bbc1d69
Kevin Kiruri <myapps255@gmail.com>
2021-08-18 17:26:54 +0300

    improvements

--- score.py
+++ score.py
@@ -3,53 +3,47 @@ def calPoints(ops) -> int:
     type ops: List[str] - List of Operators
     rtype: int - Sum of scores after performing all operations 
     """
-    scoreset = range(100)
+
     result = 0
 
     scores = list()
-    print(ops)
 
     for value in ops:
-        print("Value is {}".format(value))
-        print(type(value) == int)
-        print(value == "c")
-        print(value == "d")
-        print(value == "+")
+        if value == 'c' or value == 'C' or value == 'd' or value == 'D' or value == '+':
+            if value == "c" or value == "C":
+                if len(scores) < 1:
+                    continue
 
-        if type(value) == int:
-            print("int detected")
-            scores.append(value)    
+                else:
+                    scores.pop()
 
-        elif value == "c":
-            print("C detected")
-            scores.pop()
+            elif value == "d" or value == "D":
+                if len(scores) < 1:
+                    continue
 
-        elif value == "d":
-            lenscore = len(scores)
-            print("D score length is {}".format(lenscore))
-            scores.append(scores[lenscore - 1] * 2)
+                else:
+                    lenscore = len(scores)
+                    scores.append(scores[lenscore - 1] * 2)
 
-        elif value == "+":
-            lenscore = len(scores)
-            print("+ score length is {}".format(lenscore))
-            scores.append(scores[lenscore - 1] + scores[lenscore - 2])
-
-        print(scores)
+            elif value == "+":
+                if len(scores) < 2:
+                    continue
+                
+                else:
+                    lenscore = len(scores)
+                    scores.append(scores[lenscore - 1] + scores[lenscore - 2])
+        
+        else:
+            value = int(value)
+            scores.append(value)
 
     for score in scores:
         result += score
 
     return result
-
-
-
+         
 if __name__ == '__main__':
-    # line = input()
-    # ops = list(line)
-    # ops = line.strip().split()
-    # ops = line.split()
-    # ops = [5, 2, "c", "d", "+"]  
-    line = input()
-    ops = list(line) 
-
-    print(calPoints(ops))
+    while True:
+        line = input()
+        ops = line.strip().split()
+        print(calPoints(ops))
